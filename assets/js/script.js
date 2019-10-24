10a232f3823ae72356b372598d42cc5eaf1c4952
Larry <richardklevan@gmail.com>
2019-10-24 13:28:13 -0700

    corrected bug on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,165 @@
 var start = document.querySelector("#start-btn")
-var questionIndex=0;
+var questionIndex = 0;
 
 var secondsLeft = 50;
 
-var $optionA= document.querySelector("#option-a");
-var $optionB= document.querySelector("#option-b");
-var $optionC= document.querySelector("#option-c");
-var $optionD= document.querySelector("#option-d");
-var title =document.querySelector("#question-line")
+var $optionA = document.querySelector("#option-a");
+var $optionB = document.querySelector("#option-b");
+var $optionC = document.querySelector("#option-c");
+var $optionD = document.querySelector("#option-d");
+var title = document.querySelector("#question-line")
 
-var score=0;
+var score = 0;
 
 var questions = [
-    
-  
-  
+
+
+
   {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+
+  {
+    title: "The the javascript 'or' operator is written ____.",
+    choices: ["OR", "||", "&&", "##"],
+    answer: "||"
+  },
+
+  {
+    title: "To add a click event to a button, the  ____ method is used",
+    choices: ["addEventListener", "useEvent", "hasEvent", "textContent"],
+    answer: "addEventListener"
+  },
+  ///etc.
+];
 
 
 
 
 
 
-  start.addEventListener("click", function(){
+start.addEventListener("click", function () {
 
 
-    populate(questionIndex);
-
-// if(questionIndex<questions.length){
 
 
- 
+  populate(questionIndex);
 
-   $optionA.addEventListener("click", function(){
-    if ($optionA.textContent === questions[questionIndex].answer){
-    
-    score=score++;
-    questionIndex=questionIndex++;
-    populate(questionIndex);
-    console.log(score);
-   }
+  // if(questionIndex<questions.length){
 
-   else populate(questionIndex++)
-   console.log(score);
-    
-    })
 
-    $optionB.addEventListener("click", function(){
-      if ($optionB.textContent === questions[questionIndex].answer){
-      
-      score=score++;
-      questionIndex=questionIndex++;
+
+
+  $optionA.addEventListener("click", function () {
+
+
+
+    if ($optionA.textContent === questions[questionIndex].answer) {
+
+      score++
+      questionIndex++;
       populate(questionIndex);
       console.log(score);
-     }
-  
-     else populate(questionIndex++)
-     console.log(score);
-      
-      })
+
+    }
+
+    else {
 
 
-      $optionC.addEventListener("click", function(){
-        if ($optionC.textContent === questions[questionIndex].answer){
-        
-        score=score++;
-        questionIndex=questionIndex++;
-        populate(questionIndex);
-        console.log("HI");
-       }
-    
-       else populate(questionIndex++)
-       console.log(score);
-        
-        })
+      questionIndex++;
+      populate(questionIndex);
 
-        $optionD.addEventListener("click", function(){
-          if ($optionD.textContent === questions[questionIndex].answer){
-          
-          score=score++;
-          questionIndex=questionIndex++;
-          populate(questionIndex);
-          console.log(score);
-         }
-      
-         else populate(questionIndex++)
-         console.log(score);
-          
-          })
+      console.log(score);
 
-  
-    
+
+    }
+
+
+  })
+
+  $optionB.addEventListener("click", function () {
+    if ($optionB.textContent === questions[questionIndex].answer) {
+
+      score++
+      questionIndex++;
+      populate(questionIndex);
+      console.log(score);
+
+    }
+
+    else {
+
+
+      questionIndex++;
+      populate(questionIndex);
+
+      console.log(score);
+
+
+    }
+  })
+
+
+  $optionC.addEventListener("click", function () {
+    if ($optionC.textContent === questions[questionIndex].answer) {
+
+      score++
+      questionIndex++;
+      populate(questionIndex);
+      console.log(score);
+
+    }
+
+    else {
+
+
+      questionIndex++;
+      populate(questionIndex);
+
+      console.log(score);
+
+
+    }
+
+  })
+
+  $optionD.addEventListener("click", function () {
+    if ($optionD.textContent === questions[questionIndex].answer) {
+
+      score++
+      questionIndex++;
+      populate(questionIndex);
+      console.log(score);
+
+    }
+
+    else {
+
+
+      questionIndex++;
+      populate(questionIndex);
+
+      console.log(score);
+
+
+    }
+
   })
 
 
 
-  
+})
+
+
+
+
 
 // title.textContent = questions[questionIndex].title;
 // $optionA.textContent=questions[questionIndex].choices[0];
@@ -120,22 +173,22 @@ var questions = [
 
 
 
- 
 
 
-    
-
-  
 
 
-function populate(i){
 
 
-title.textContent = questions[i].title;
-$optionA.textContent=questions[i].choices[0];
-$optionB.textContent=questions[i].choices[1];
-$optionC.textContent=questions[i].choices[2];
-$optionD.textContent=questions[i].choices[3];
 
 
- }
+function populate(i) {
+
+
+  title.textContent = questions[i].title;
+  $optionA.textContent = questions[i].choices[0];
+  $optionB.textContent = questions[i].choices[1];
+  $optionC.textContent = questions[i].choices[2];
+  $optionD.textContent = questions[i].choices[3];
+
+
+}
